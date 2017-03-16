cec7e898795c8fbffe2f25406bc90b151feb9651
Peter Springer <peterspringer829@gmail.com>
2017-03-16 12:06:26 -0600

    update page state on button click

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      names: ['pete', 'jeff', 'ben', 'david', 'kirsten', 'jacky']
+      names: ['pete', 'jeff', 'ben', 'david', 'kirsten', 'jacky'],
+      page: 1
     }
   }
 
@@ -18,31 +19,19 @@ class App extends Component {
     })
   }
 
-  renderListings() {
-  return this.props.userData.map(user => {
-    return (
-      <article className="listing" key={user.email}>
-      <p>Name: {user.name}</p>
-      <p>School: {user.school}</p>
-      <p>Employer: {user.company}</p>
-      <p>Location: {user.location}</p>
-      <img className='user-photo'src={user.photoURL} alt='user photo'/>
-      </article>
-    );
-  });
-};
+  setPage(e) {
+    this.setState({page: e.target.innerText})
+  }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Basic Pagination</h2>
         </div>
         {this.renderNames(this.state.names)}
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={(e) => this.setPage(e)}>1</button><button onClick={(e) => this.setPage(e)}>2</button>
       </div>
     );
   }
